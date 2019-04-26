ce2cd46f8d4fed5b05d38b285b9bb3881f009822
junkito14 <49921260+junkito14@users.noreply.github.com>
2019-04-26 14:20:27 -0500

    Update cod_proyecto.py

--- cod_proyecto.py
+++ cod_proyecto.py
@@ -1,4 +1,6 @@
 import random 
+import math
+import numpy as np
 
 def create_numbers ():
     """ Esta función sirve para generar números aleatorios entre 0 y 1""" 
@@ -10,3 +12,25 @@ def create_numbers ():
     return numbers
 
 print (create_numbers())
+
+def get_rand_number(val_max, val_min)
+    """Esta función genera números aleatorios utilizando una distribución
+    uniforme para que todos tengas igual probabilidad entre dos valores
+    asignados"""
+    range = val_max - val_min
+    elecc = random.uniform(0,1)
+    return val_min + range*elecc
+
+def first_montecarlo(num=10000):
+    """Esta función realiza un montacarlo inicial, probando que funcione
+    el método"""
+    limite_inferior = 0
+    limite_superior = 2
+   
+sum_ejemplos = 0
+for i in range(num_ejemplos):
+    x = get_rand_number(limite_inferior, limite_superior)
+    sum_ejemplos += f_de_x(x)
+
+return (limite_superior - limite inferior) * float(sum_ejemplos/num_ejemplos)
+    
